deae78ed706b747f65f70f68f77e1a8787e526b7
Ryan C. Gordon <icculus@icculus.org>
2011-05-07 03:58:34 -0400

    Make buffer sizes unsigned.

--- mojodds.c
+++ mojodds.c
@@ -173,8 +173,8 @@ static int parse_dds(MOJODDS_Header *header, const uint8 **ptr, size_t *len)
 
 
 // !!! FIXME: improve the crap out of this API later.
-int MOJODDS_getTexture(const void *_ptr, const long _len,
-                       const void **_tex, long *_texlen, int *_dxtver)
+int MOJODDS_getTexture(const void *_ptr, const unsigned long _len,
+                       const void **_tex, unsigned long *_texlen, int *_dxtver)
 {
     size_t len = (size_t) _len;
     const uint8 *ptr = (const uint8 *) _ptr;
