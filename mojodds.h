786dff68e524579bd393ff7474ce7b9cd608bdb6
Ryan C. Gordon <icculus@icculus.org>
2011-05-07 23:12:10 -0400

    Added mojodds.h

---
+++ mojodds.h
@@ -0,0 +1,14 @@
+#ifndef _INCL_MOJODDS_H_
+#define _INCL_MOJODDS_H_
+
+/* !!! FIXME: flesh this file out. */
+
+int MOJODDS_getTexture(const void *_ptr, const unsigned long _len,
+                       const void **_tex, unsigned long *_texlen, int *_dxtver,
+                       unsigned int *_w, unsigned int *_h);
+
+#endif
+
+/* end of mojodds.h ... */
+
+
