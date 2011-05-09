a82e6f87f81e6ce5f7cbe900d3fd6ad594bdd447
Ryan C. Gordon <icculus@icculus.org>
2011-05-08 21:34:49 -0400

    Added #ifdef __cplusplus blocks.

--- mojodds.h
+++ mojodds.h
@@ -2,14 +2,20 @@
 #define _INCL_MOJODDS_H_
 
 /* !!! FIXME: flesh this file out. */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 int MOJODDS_isDDS(const void *_ptr, const unsigned long _len);
 int MOJODDS_getTexture(const void *_ptr, const unsigned long _len,
                        const void **_tex, unsigned long *_texlen, int *_dxtver,
                        unsigned int *_w, unsigned int *_h);
 
+#ifdef __cplusplus
+}
+#endif
+
 #endif
 
 /* end of mojodds.h ... */
-
 
